2a3b2086bca251ab199966b7e86ca8fe37d85e57
Leah Hanson <astrieanna@gmail.com>
2013-07-16 12:05:52 -0400

    started using bootstrap styles

--- main.jl
+++ main.jl
@@ -1,16 +1,28 @@
-using HttpServer, DataFrames, Calendar, SQLite, Mustache
+using HttpServer, DataFrames, Calendar, SQLite, Mustache, HttpCommon
 require("templates.jl")
 
 println("connecting to db")
 conn = connect("blog.db")
 posts = query("select * from posts",conn)
-posts["url"] = map(str -> replace(str," ","%20"),posts["title"])
+posts["url"] = map(encodeURI,posts["title"])
 
-println("about to run")
-run(Server((req,res)-> begin 
+function myhandler(req,res)
+  # Index Page
   if req.resource == "/" || req.resource == ""
     return Response(render(index_tmpl,{"title"=>"Contents","d"=>posts}))
   end
+  # Bootstrap css/js files
+  if ismatch(r"^/bootstrap/",req.resource)
+    f = open(".$(req.resource)")
+    h = HttpCommon.headers() #need to set headers correctly
+    if ismatch(r".css$",req.resource)
+      h["Content-Type"] = "text/css; charset=utf-8"
+    elseif ismatch(r".css$",req.resource)
+      h["Content-Type"] = "text/js; charset=utf-8"
+    end
+    return Response(readall(f),h)
+  end
+  # Posts
   i = 0 
   for name in posts["url"]
     i += 1
@@ -23,4 +35,7 @@ run(Server((req,res)-> begin
     end
   end
   return Response(404)
- end),8080)
+end
+
+println("about to run")
+run(Server(myhandler),8080)
--- templates.jl
+++ templates.jl
@@ -1,23 +1,41 @@
 header_tmpl = "
+<!DOCTYPE html>
 <html>
 <head>
 <title>{{title}}</title>
+<link href=\"./bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">
+<style type=\"text/css\">
+  body {
+    padding-top: 60px;
+    padding-bottom: 40px;
+  }
+</style>
 </head>
+
 <body>
+<div class=\"container\">
 "
+
 footer_tmpl = "
+</div> 
 </body>
 </html>
 " 
 
 index_tmpl = "$header_tmpl
+<div class=\"hero-unit\">
 <h1>{{title}}</h1>
-<table>
-<tr><th>name</th><th>summary</th></tr>
+</div>
+<div class=\"row\">
+  <div class=\"span4\">name</div>
+  <div class=\"span8\">summary</div>
+</div>
 {{#d}}
-<tr><td><a href={{url}}>{{title}}</a></td><td>{{summary}}</td></tr>
+<div class=\"row\">
+  <div class=\"span4\"><a href=\"{{url}}\">{{title}}</a></div>
+  <div class=\"span8\">{{summary}}</div>
+</div>
 {{/d}}
-</table>
 $footer_tmpl"
 
 post_tmpl = "$header_tmpl
